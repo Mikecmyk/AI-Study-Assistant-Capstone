cab6c8567a21e40cdd977658337b53cb4427d2bf
mikecmyk <michaelmwaura846@gmail.com>
2025-10-28 14:01:26 +0300

    fix: Auto-populate slug field in StudyTopicAdmin to allow data entry

--- study_core/admin.py
+++ study_core/admin.py
@@ -2,5 +2,9 @@
 from django.contrib import admin
 from .models import StudyTopic, StudySession
 
-admin.site.register(StudyTopic)
-admin.site.register(StudySession)
+class StudyTopicAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('name', 'slug') 
+
+admin.site.register(StudyTopic, StudyTopicAdmin) 
+admin.site.register(StudySession) 
