d7f435a036f2731c95a9ab03b63168a4743bc59d
mikecmyk <michaelmwaura846@gmail.com>
2025-10-28 13:44:19 +0300

    Registered StudyTopic and StudySession models with Django Admin

--- study_core/admin.py
+++ study_core/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import StudyTopic, StudySession
+
+admin.site.register(StudyTopic)
+admin.site.register(StudySession)
