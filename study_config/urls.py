1279960a079e0a27b75eddc9d5deb427f54416e9
mikecmyk <michaelmwaura846@gmail.com>
2025-11-06 15:12:06 +0300

    Registered 'users' app and updated project/app URLs

--- study_config/urls.py
+++ study_config/urls.py
@@ -2,14 +2,16 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import RedirectView # Import for redirecting root
+from django.views.generic.base import RedirectView 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    # API endpoints for the core app
+
     path('api/', include('study_core.urls')), 
     
-    # Redirect root path (/) to the API for easy testing during development
+    path('api/auth/', include('users.urls')),
+
+    
     path('', RedirectView.as_view(url='api/', permanent=True)), 
 ]
